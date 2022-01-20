2287f279086942a834582fbc7f543e11195678df
bag19942328 <97687686+bag19942328@users.noreply.github.com>
2022-01-20 11:30:47 -0500

    Personal comp1

--- labs/Lab1/js/app.js
+++ labs/Lab1/js/app.js
@@ -1,9 +1,46 @@
+//Brandon Green 
+//1/20/2022
+//N220
 
+//application level variables
+let size = 10;
+
+
+//setup the applicaiton 
 function setup() {
-    createCanvas(600, 400);
+    //create canvas of size 800 - big enough to draw on 
+    createCanvas(800, 600);
+
+    /*
+    stroke(255, 255, 255);
+    strokeWeight(10);
+
+    fill(255, 99, 99);
+    circle(150, 150, 200);
+    fill(186, 255, 180);
+    circle(150, 150, 150);
+    fill(255, 171, 118);
+    circle(150, 150, 50);
+    */
+
+    console.log(size);
 }
 
 function draw() {
-    fill(255, 0, 0);
-    circle(mouseX, mouseY, frameCount / 50);
+
+    //increase size
+
+    console.log(size);
+
+
+    //draw a circle at the center at an increasing size 
+    circle(150, 150, size);
+
+
+}
+
+function mousePressed() {
+    if (size < 50) {
+        size += 1;
+    }
 }
